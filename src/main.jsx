f366f70130106e1a16c24432e44b65ac95e3b38b
Nandalal Shukla <shuklanandalal@gmail.com>
2025-10-06 01:00:37 +0530

    created a 3d cube using gsap and claude sonnet 4,5

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Board from "./App.jsx";
+import Box from "./box.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="flex justify-center items-center min-h-screen min-w-screen">
       <Board />
+      <Box />
     </div>
   </StrictMode>
 );
